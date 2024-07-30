eac4a236723cb9d28c6c8b343beb617d9c0e635a
Sanjay Baitha <91378949+rajakprogrammer@users.noreply.github.com>
2024-07-30 11:18:30 +0530

    Time: 0 ms (100.00%), Space: 12.6 MB (5.53%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(),citations.rend());
+        int h=0;
+        while(h<citations.size() && citations[h] > h){
+            h++;
+        }
+        
+        return h;
+        
+    }
+};
